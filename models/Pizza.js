e3601043e442c28b80e87f439555529ca8caca05
Sandra Sanders <sandy-sanders@msn.com>
2022-04-22 21:12:04 -0700

    find all pizza data'

---
+++ models/Pizza.js
@@ -0,0 +1,26 @@
+const { schema, model } = require('mongoose');
+
+
+const Pizzaschema = new schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
