298d8881ad70ea2f840a98c40351e78b9e715e7a
Sandra Sanders <sandy-sanders@msn.com>
2022-04-26 13:43:16 -0700

    added comments to get request

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const PizzaRoutes = require('./pizza-routes');
-
+const commentRoutes = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', PizzaRoutes);
-
+router.use('/comments', commentRoutes);
 
 
 
